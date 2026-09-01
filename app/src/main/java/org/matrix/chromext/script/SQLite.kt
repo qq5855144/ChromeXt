bdf1a86c6313310a0bf7817c67e437ee09b6da28
练习时长两年半的水怪 <140933838+qq5855144@users.noreply.github.com>
2026-09-01 21:51:28 +0800

    feat: persist userscript enabled state

--- app/src/main/java/org/matrix/chromext/script/SQLite.kt
+++ app/src/main/java/org/matrix/chromext/script/SQLite.kt
@@ -18,10 +18,11 @@ data class Script(
     val runAt: String = "document-idle",
     val injectInto: String = "page",
     val sandbox: String = "raw",
+    var enabled: Boolean = true,
 )
 
 private const val SQL_CREATE_ENTRIES =
-    "CREATE TABLE script (id TEXT PRIMARY KEY NOT NULL, meta TEXT NOT NULL, code TEXT NOT NULL, storage TEXT);"
+    "CREATE TABLE script (id TEXT PRIMARY KEY NOT NULL, meta TEXT NOT NULL, code TEXT NOT NULL, storage TEXT, enabled INTEGER NOT NULL DEFAULT 1);"
 
 class ScriptDbHelper(context: Context) :
     SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
@@ -46,11 +47,16 @@ class ScriptDbHelper(context: Context) :
     if (oldVersion == 8 && newVersion == 9) {
       db.execSQL("CREATE TABLE tmp AS SELECT id,meta,code,storage FROM script;")
       db.execSQL("DROP TABLE script;")
-      db.execSQL(SQL_CREATE_ENTRIES)
+      db.execSQL(
+          "CREATE TABLE script (id TEXT PRIMARY KEY NOT NULL, meta TEXT NOT NULL, code TEXT NOT NULL, storage TEXT);")
       db.execSQL(
           "INSERT INTO script (id,meta,code) SELECT id,meta,code FROM tmp WHERE storage = '';")
       db.execSQL("INSERT INTO script SELECT * FROM tmp WHERE storage != '';")
       db.execSQL("DROP TABLE tmp;")
+    }
+
+    if (oldVersion == 9 && newVersion == 10) {
+      db.execSQL("ALTER TABLE script ADD COLUMN enabled INTEGER NOT NULL DEFAULT 1;")
     }
 
     if (newVersion - oldVersion > 1) {
@@ -62,7 +68,7 @@ class ScriptDbHelper(context: Context) :
   override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
 
   companion object {
-    const val DATABASE_VERSION = 9
+    const val DATABASE_VERSION = 10
     const val DATABASE_NAME = "userscript"
   }
 }
